4a300bc7f463f8d5d3aee0b38c5f47c417ea929f
Hashim Razak <140894330+4nt4g0n15t@users.noreply.github.com>
2024-08-16 11:52:10 +0000

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-echo "Running StealthPent Workflow..."
+# Choose mode: standard or paranoid
+mode=$1
 
-bash scripts/selected/scan_ports.sh
-bash scripts/selected/enumerate_services.sh
-bash scripts/selected/vulnerability_scan.sh
-bash scripts/selected/recon_network.sh
-bash scripts/selected/gather_osint.sh
+# Choose scan type: all or custom ports
+scan_type=$2
 
-echo "Workflow completed. Check the results in the results/scan_results/ directory."
+if [ "$mode" == "standard" ]; then
+    bash scripts/standard/scan_ports.sh $scan_type
+    bash scripts/standard/enumerate_services.sh $scan_type
+    bash scripts/standard/vulnerability_scan.sh $scan_type
+    bash scripts/standard/recon_network.sh
+    bash scripts/standard/gather_osint.sh
+elif [ "$mode" == "paranoid" ]; then
+    bash scripts/paranoid/scan_ports.sh $scan_type
+    bash scripts/paranoid/enumerate_services.sh $scan_type
+    bash scripts/paranoid/vulnerability_scan.sh $scan_type
+    bash scripts/paranoid/recon_network.sh
+    bash scripts/paranoid/gather_osint.sh
+else
+    echo "Invalid mode. Please choose 'standard' or 'paranoid'."
+fi

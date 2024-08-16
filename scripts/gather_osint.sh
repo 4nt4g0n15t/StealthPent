9a8f44961d0666fb6f50217a56f2d4454f28e746
Hashim Razak <140894330+4nt4g0n15t@users.noreply.github.com>
2024-08-16 11:49:22 +0000

    Delete scripts/gather_osint.sh

--- scripts/gather_osint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while IFS= read -r target
-do
-    ip=$(echo $target | cut -d':' -f1)
-    
-    # OSINT Gathering with theHarvester
-    theHarvester -d $ip -b all > results/scan_results/${ip}_osint.txt
-done < config/targets.txt

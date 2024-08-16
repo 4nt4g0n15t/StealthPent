9e45fa4f195d610c0776f473e8bc23d1038a307e
Hashim Razak <140894330+4nt4g0n15t@users.noreply.github.com>
2024-08-16 11:41:34 +0000

    Update recon_network.sh

--- scripts/paranoid/recon_network.sh
+++ scripts/paranoid/recon_network.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+while IFS= read -r target
+do
+    ip=$(echo $target | cut -d':' -f1)
+    
+    # Example recon tools
+    whois $ip > results/scan_results/${ip}_whois.txt
+    dig -x $ip > results/scan_results/${ip}_reverse_dns.txt
+    traceroute $ip > results/scan_results/${ip}_traceroute.txt
+done < config/targets.txt
 
